44fd957f862801c4e561c9471e69a39153a1a6f2
Gervais Naoussi <gervaisnaoussi@gmail.com>
2015-12-08 15:44:18 +0100

    document_page code coverage improved

--- document_page/tests/test_document_page_create_menu.py
+++ document_page/tests/test_document_page_create_menu.py
@@ -10,8 +10,9 @@ class TestDocumentPageCreateMenu(common.TransactionCase):
         """Test page menu creation."""
         menu_parent = self.env.ref('base.menu_base_partner')
 
-        menu_created = self.env['document.page.create.menu'].create({
-             'menu_name': 'Wiki Test menu', 'menu_parent_id': menu_parent.id})
+        menu_created = self.env['document.page.create.menu'].create(
+            {'menu_name': 'Wiki Test menu', 'menu_parent_id': menu_parent.id}
+        )
 
         menu = self.env['document.page.create.menu'].search(
             [('id', '=', menu_created.id)]
@@ -19,3 +20,11 @@ class TestDocumentPageCreateMenu(common.TransactionCase):
         menu.with_context(
             active_id=[self.ref('document_page.demo_page1')]
         ).document_page_menu_create()
+
+        fields_list = ["menu_name", "menu_name"]
+
+        res = menu.with_context(
+            active_id=[self.ref('document_page.demo_page1')]
+        ).default_get(fields_list)
+
+        self.assertEqual(res['menu_name'], 'OpenERP 6.1. Functional Demo')
